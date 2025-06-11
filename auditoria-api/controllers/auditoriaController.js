7be079dfea16d51cab767740e21986c8fec68af3
ikenyp <kenyeto11@gmail.com>
2025-06-11 16:09:34 -0500

    Controller

---
+++ auditoria-api/controllers/auditoriaController.js
@@ -0,0 +1,20 @@
+import Auditoria from "../models/Auditoria";
+
+export const crearRegistro = async (req, res) => {
+    try {
+        const nuevo = new Auditoria(req.body);
+        const guardado = await nuevo.save();
+        res.status(201).json(guardado);
+        } catch (err) {
+          res.status(500).json({ error: err.message });
+        }
+};
+
+export const obtenerRegistros = async (req, res) => {
+    try {
+        const registros = await Auditoria.find();
+        res.status(200).json(registros);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
