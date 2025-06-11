9cb9f761163df16343af03eee29ddb7372d83d64
ikenyp <kenyeto11@gmail.com>
2025-06-11 16:00:57 -0500

    Creacion de index

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+// Convierte JSON entrante
+app.use(express.json());
+
+
+// Conexion a la base de datos
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Conectado a MongoDB');
+    app.listen(process.env.PORT, () => 
+        console.log(`Servidor escuchando en el puerto ${process.env.PORT}`)
+    );
+}).catch(err => {
+    console.error('Error al conectar a MongoDB:', err);
